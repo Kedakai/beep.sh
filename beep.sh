f9fcb4d8893d8082d105149d70b72d8c125a31d1
Kedakai <kibaslp@gmail.com>
2017-01-17 02:54:06 +0100

    Create beep.sh

---
+++ beep.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+/usr/local/bin/beep -p 150 60
+sleep 0.05
+/usr/local/bin/beep -p 150 60
+sleep 0.05
+/usr/local/bin/beep -p 150 60
+sleep 0.05
+/usr/local/bin/beep -p 230 60
+sleep 0.05
+/usr/local/bin/beep -p 340 60
+sleep 0.05
+/usr/local/bin/beep -p 340 60
+sleep 0.05
+/usr/local/bin/beep -p 340 60
+sleep 0.05
+/usr/local/bin/beep -p 300 60
+sleep 0.05
+/usr/local/bin/beep -p 230 60
+sleep 0.05
+/usr/local/bin/beep -p 230 60
+sleep 0.05
+/usr/local/bin/beep -p 230 60
+sleep 0.05
+/usr/local/bin/beep -p 230 60
+sleep 0.05
+/usr/local/bin/beep -p 120 60
+sleep 0.05
+/usr/local/bin/beep -p 120 60
+sleep 0.05
+/usr/local/bin/beep -p 120 60
+sleep 0.05
+/usr/local/bin/beep -p 80  60
+sleep 0.6
+/usr/local/bin/beep -p 400 30 
+/usr/local/bin/beep -p 400 30 
+/usr/local/bin/beep -p 340 30 
+/usr/local/bin/beep -p 400 30 
+/usr/local/bin/beep -p 340 30 
+/usr/local/bin/beep -p 400 30 
+/usr/local/bin/beep -p 460 30 
+/usr/local/bin/beep -p 500 55
+/usr/local/bin/beep -p 400 30 
+/usr/local/bin/beep -p 400 30 
+/usr/local/bin/beep -p 300 30
+/usr/local/bin/beep -p 500 135
+sleep 0.2
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 370 80
+/usr/local/bin/beep -p 370 30
+/usr/local/bin/beep -p 370 30
+/usr/local/bin/beep -p 340 30
+/usr/local/bin/beep -p 400 30
+sleep 0.6
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 340 30
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 340 30
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 460 30
+/usr/local/bin/beep -p 500 55
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 600 30
+/usr/local/bin/beep -p 500 135
+sleep 0.2
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 400 30
+/usr/local/bin/beep -p 460 60
+sleep 0.6
+/usr/local/bin/beep -p 500 30
+/usr/local/bin/beep -p 500 30
+/usr/local/bin/beep -p 500 30
